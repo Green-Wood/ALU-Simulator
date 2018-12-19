1aeaac0551f1da17747f4b5d3158466c8f6a1d18
赵文祺 <1027572886a@gmail.com>
2018-12-19 20:13:47 +0800

    测试用例 Dec 19, 2018, 20:13

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import Basic.StringGenerator;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Integer.valueOf("00" + StringGenerator.repeat('1', 30), 2));
     }
 }
--- src/test/java/IntegerNumberTest.java
+++ src/test/java/IntegerNumberTest.java
@@ -22,11 +22,13 @@ public class IntegerNumberTest {
 
     @Test
     public void multi() {
-        int ran1 = (int) (Math.random() * -1000 + 500);
-        int ran2 = (int) (Math.random() * -1000 + 500);
-        test.setOperand(String.valueOf(ran1), String.valueOf(ran2));
-        assertEquals(String.valueOf(ran1 * ran2), test.multi());
-        assertEquals(String.valueOf(7 * 3), test.setOperand("7", "3").multi());
+        for (int i = 0; i < 100; i++) {
+            int ran1 = (int) (Math.random() * -1000 + 500);
+            int ran2 = (int) (Math.random() * -1000 + 500);
+            test.setOperand(String.valueOf(ran1), String.valueOf(ran2));
+            assertEquals(String.valueOf(ran1 * ran2), test.multi());
+            assertEquals(String.valueOf(7 * 3), test.setOperand("7", "3").multi());
+        }
     }
 
     @Test
@@ -35,9 +37,11 @@ public class IntegerNumberTest {
         assertArrayEquals(new String[]{"-2", "0"}, test.division());
         test.setOperand("-4", "-2");
         assertArrayEquals(new String[]{"2", "0"}, test.division());
+        test.setOperand("9", "-9");
+        assertArrayEquals(new String[]{"-1", "0"}, test.division());
         for (int i = 0; i < 100; i++) {
-            int ran1 = (int) (Math.random() * -1000 + 500);
-            int ran2 = (int) (Math.random() * -1000 + 500);
+            int ran1 = (int) (Math.random() * -100000 + 50000);
+            int ran2 = (int) (Math.random() * -100000 + 50000);
             String[] expect = new String[]{String.valueOf(ran1 / ran2), String.valueOf(ran1 % ran2)};
             test.setOperand(String.valueOf(ran1), String.valueOf(ran2));
             System.out.println("ran1: " + ran1 + " ran2: " + ran2);
